33f4933b7994a2a581975972fcbc3453aef11175
fullyalive <wogud1202@gmail.com>
2019-03-26 13:26:36 +0900

    withRouter를 이용해 location aware Header 만들기

--- README.md
+++ README.md
@@ -1,10 +1,12 @@
 # **スゴイ映画** (스고이영화)
 
-React & ES6 - 뭔가 일본스러운 UI/UX 영화 서비스 🐾
+뭔가 일본스러운 UI/UX 영화 서비스 🐾
+- React
+- styled-components
 
 ## Screens
 
 - 메인 화면 (Home)
 - 티비 프로그램 (TV Shows)
 - 검색 (Search)
-- 영상 세부정보 (Detail)
+- 세부정보 (Detail)
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, withRouter } from "react-router-dom";
 import styled from "styled-components";
 
 const Header = styled.header``;
@@ -12,23 +12,34 @@ const List = styled.ul`
   }
 `;
 
+const Item = styled.li`
+  padding-bottom: 3px;
+  margin-right: 10px;
+  border-bottom: 3px solid
+    ${props => (props.current ? "#1071FF" : "transparent")};
+  transition: border-bottom 0.3s ease-in-out;
+  &:last-child {
+    margin-right: 0;
+  }
+`;
 
-const Item = styled.li``;
-const ALink = styled(Link)``; 
+const ALink = styled(Link)``;
 // react-router-dom에서 불러온 Link를 styled-components를 이용해 꾸며줄 수 있다.
 
-export default () => (
+export default withRouter(({ location: { pathname } }) => (
+  // withRouter를 통해 컴포넌트에 접근해 props를 얻어낸다.
   <Header>
+    {/* {console.log(props)} */}
     <List>
-      <Item>
+      <Item current={pathname === "/"}>
         <ALink to="/">Movies</ALink>
       </Item>
-      <Item>
+      <Item current={pathname === "/tv"}>
         <ALink to="/tv">TV</ALink>
       </Item>
-      <Item>
+      <Item current={pathname === "/search"}>
         <ALink to="/search">Search</ALink>
       </Item>
     </List>
   </Header>
-);
+));
