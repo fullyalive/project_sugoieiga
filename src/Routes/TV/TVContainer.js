1307db969904209dd349cb6477fab8dbff194537
fullyalive <wogud1202@gmail.com>
2019-03-27 18:15:05 +0900

    TV Container state 작업

--- src/Routes/TV/TVContainer.js
+++ src/Routes/TV/TVContainer.js
@@ -1,24 +1,39 @@
 import React from "react";
 import TVPresenter from "./TVPresenter";
+import { tvApi } from "api";
 
 export default class extends React.Component {
   state = {
-    nowPlaying: null,
-    upcoming: null,
+    airingToday: null,
     topRated: null,
     popular: null,
     error: null,
     loading: true
   };
 
+  async componentDidMount() {
+    try {
+      const {
+        data: { results: airingToday }
+      } = await tvApi.airingToday();
+      const {
+        data: { results: topRated }
+      } = await tvApi.topRated();
+      const {
+        data: { results: popular }
+      } = await tvApi.popular();
+      this.setState({ airingToday, topRated, popular });
+    } catch {
+      this.setState({
+        error: "放送情報の読み込みに失敗しました　😢"
+      });
+    } finally {
+      this.setState({ loading: false });
+    }
+  }
+
   render() {
-    const {
-      airingToday,
-      topRated,
-      popular,
-      error,
-      loading
-    } = this.state;
+    const { airingToday, topRated, popular, error, loading } = this.state;
     return (
       <TVPresenter
         airingToday={airingToday}
