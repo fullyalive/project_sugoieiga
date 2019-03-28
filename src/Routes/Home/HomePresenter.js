b8df97ebb05174ea0cd2fa1f48de186faa640d7a
fullyalive <wogud1202@gmail.com>
2019-03-28 17:23:24 +0900

    Presenter 구조 셋업

--- src/Routes/Home/HomePresenter.js
+++ src/Routes/Home/HomePresenter.js
@@ -1,1 +1,23 @@
-export default () => "Home";
+import React from "react";
+import PropTypes from "prop-types";
+import styled from "styled-components";
+
+const HomePresenter = ({
+  nowPlaying,
+  upcoming,
+  topRated,
+  popular,
+  error,
+  loading
+}) => null;
+
+HomePresenter.PropTypes = {
+  nowPlaying: PropTypes.array,
+  upcoming: PropTypes.array,
+  topRated: PropTypes.array,
+  popular: PropTypes.array,
+  error: PropTypes.string,
+  loading: PropTypes.bool.isRequired
+};
+
+export default HomePresenter;
